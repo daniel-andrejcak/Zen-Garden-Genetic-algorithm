e5d4eed74ddae3cbb7c0057e60c6acd030239e21
Daniel Andrejcak <danielandrejcak@gmail.com>
2023-10-20 23:32:54 +0200

    asi funguje vypiasnie genov na hraciu plochu

--- Header.h
+++ Header.h
@@ -1,17 +1,48 @@
 #pragma once
 
+unsigned short X = 0;
+unsigned short Y = 0;
+unsigned short stoneCount = 0;
+unsigned short populationSize = 50;
+unsigned short individualSize = 0;
+unsigned short desiredFitnesValue = 0;
+
+class Individual
+{
+public:
+	Individual()
+	{
+		genes = std::vector<unsigned short>(X + Y + stoneCount);
+	};
+
+
+	~Individual() {};
+
+	unsigned short fitnesValue = 0;
+
+	std::vector<unsigned short> genes;
+
+
+	bool operator!=(const Individual& other) const {
+		return this->fitnesValue != other.fitnesValue;
+	}
+
+};
+
 
 class Garden
 {
 public:
 	
-	std::vector<std::vector<unsigned short>> grid;
+	std::vector<std::vector<short>> grid;
 
-	Garden(unsigned short x, unsigned short y)
+	Garden(){}
+
+	Garden(short x, short y)
 	{
 		for (size_t i = 0; i < x; i++)
 		{
-			std::vector<unsigned short> xAxis(y);
+			std::vector<short> xAxis(y);
 			
 			grid.push_back(xAxis);
 		}
@@ -20,17 +51,151 @@ public:
 	~Garden() {};
 
 
-	void placeStone(unsigned short x, unsigned short y)
+	void moveDown(unsigned short x, unsigned short yStart, unsigned short moveNumber, const Individual& individual)
+	{
+		for (unsigned short y = yStart; y < Y; y++)
+		{
+			if (!grid[x - 1][y])
+			{
+				grid[x - 1][y] = moveNumber;
+			}
+			else
+			{
+				if(individual.genes[X + Y + ((moveNumber + 0) % stoneCount)] == 0 && x != X && !grid[x][y - 1])
+				{
+					moveRight(x, y, moveNumber, individual);
+				}
+				else if(x - 1 != 0 && !grid[x - 2][y - 1])
+				{
+					moveLeft(x - 2, y, moveNumber, individual);
+				}
+
+				return;
+			}
+		}
+	}
+
+	void moveUp(unsigned short x, unsigned short yStart, unsigned short moveNumber, const Individual& individual)
+	{
+		for (short y = yStart; y >= 0; y--)
+		{
+			if (!grid[x - 1][y])
+			{
+				grid[x - 1][y] = moveNumber;
+			}
+			else
+			{
+				if (individual.genes[X + Y + ((moveNumber + 1) % stoneCount)] == 0 && x - 1 != 0 && !grid[x - 2][y + 1])
+				{
+					moveLeft(x - 2, y + 2, moveNumber, individual);
+				}
+				else if (x != X && !grid[x][y + 1])
+				{
+					moveRight(x, y + 2, moveNumber, individual);
+				}
+
+				return;
+			}
+		}
+	}
+
+	void moveLeft(unsigned short xStart, unsigned short y, unsigned short moveNumber, const Individual& individual)
+	{
+		for (short x = xStart; x >= 0; x--)
+		{
+			if (!grid[x][y - 1])
+			{
+				grid[x][y - 1] = moveNumber;
+			}
+			else
+			{
+				if (individual.genes[X + Y + ((moveNumber + 2) % stoneCount)] == 0 && y != Y && !grid[x + 1][y])
+				{
+					moveDown(x + 2, y, moveNumber, individual);
+				}
+				else if (y - 1 != 0 && !grid[x + 1][y - 2])
+				{
+					moveUp(x + 2, y - 2, moveNumber, individual);
+				}
+
+				return;
+			}
+		}
+	}
+
+	void moveRight(unsigned short xStart, unsigned short y, unsigned short moveNumber, const Individual& individual)
+	{
+		for (unsigned short x = xStart; x < X; x++)
+		{
+			if (!grid[x][y - 1])
+			{
+				grid[x][y - 1] = moveNumber;
+			}
+			else
+			{
+				if (individual.genes[X + Y + ((moveNumber + 2) % stoneCount)] == 0 && y - 1 != 0 && !grid[x - 1][y - 2])
+				{
+					moveUp(x, y - 2, moveNumber, individual);
+				}
+				else if (y != Y && !grid[x - 1][y])
+				{
+					moveDown(x, y, moveNumber, individual);
+				}
+
+				return;
+			}
+		}
+	}
+
+
+	//funkcia pouzije geny - pohyby mnicha na garden
+	void useGenes(const Individual& individual)
+	{
+		/*ak je cislo mensie ako X tak je to pohyb z hornej strany,
+		  ak je cislo vacsie ako X a mensie rovne X + Y, tak je to pohyb z pravej strany,
+		  ak je cislo vacsie ako X + Y a mensie rovne ako X + Y + X, tak je to pohyb zo spodnej strany
+		  ak je cislo vacsie ako X + Y + X a mensie rovne ako X + Y + X + Y tak je to pohyb z lavej strany*/
+	
+		for (unsigned short i = 0; i < X + Y; i++)
+		{
+			if (individual.genes[i] == 0)
+			{
+				continue;
+			}
+
+			if (individual.genes[i] <= X)
+			{
+				if (!grid[individual.genes[i] - 1][0])
+					moveDown(individual.genes[i], 0, i + 1, individual);
+			}
+			else if (individual.genes[i] <= X + Y)
+			{
+				if(!grid[X - 1][(individual.genes[i] - X) - 1])
+					moveLeft(X - 1, individual.genes[i] - X, i + 1, individual);
+			}
+			else if (individual.genes[i] <= X + Y + X)
+			{
+				if(!grid[(individual.genes[i] - X - Y) - 1][Y - 1])
+					moveUp(individual.genes[i] - X - Y, Y - 1, i + 1, individual);
+			}
+			else if (individual.genes[i] <= X + Y + X + Y)
+			{
+				if (!grid[0][(individual.genes[i] - X - Y - X) - 1])
+					moveRight(0, individual.genes[i] - X - Y - X, i + 1, individual);
+			}
+		}
+	}
+
+	
+
+
+	void placeStone(short x, short y)
 	{
 		grid[x][y] = -1;
+
+		stoneCount += 1;
 	}
 };
 
-class Genes
-{
-public:
-	Genes() {};
-	~Genes() {};
 
 
-};
--- main.cpp
+++ main.cpp
@@ -1,22 +1,25 @@
-#include<iostream>
-#include<vector>
 #include<algorithm>
+#include<iostream>
+#include<iomanip>
+#include<random>
+#include<utility>
+#include<vector>
 
 #include"Header.h"
 
 
-unsigned short X = 0;
-unsigned short Y = 0;
+Garden blankGarden; //globalny objekt, v ktorom budu len kamene, je to podklad pre vytvorenie ostatnych objektov
+std::vector<unsigned short> blankGenes; //globalny objekt genov, ktory sa bude pouzivat na random generovanie prvej generacie
 
 
 //vypis zahrady
-std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<unsigned short>>& vec)
+std::ostream& operator<<(std::ostream& os, const Garden& garden)
 {
 	for (size_t y = 0; y < Y; y++)
 	{
 		for (size_t x = 0; x < X; x++)
 		{
-			os << vec[x][y] << ' ';
+			os << std::setw(3) << garden.grid[x][y] << ' ';
 		}
 
 		os << std::endl;
@@ -26,37 +29,240 @@ std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<unsigne
 }
 
 
-unsigned char fitnes(const Garden& garden, const Genes& genes)
+//lepsi random generator indexov, najdene na cppreference.com
+std::random_device rd;  // a seed source for the random number engine
+std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
+
+unsigned short randIndex(unsigned short min, unsigned short max)
 {
-	return 0;
+	std::uniform_int_distribution<> distribution(min, max);
+
+	return distribution(gen);
 }
 
+
+//fitnes funkcia ohodnoti dany gen - spocita pocet pohrabanych policiek
+unsigned short fitnes(const Individual& individual)
+{
+	if (individual.fitnesValue != 0)
+		return individual.fitnesValue;
+
+	//z jedinca sa spravi zahrada, ktora sa ohodnoti
+	Garden garden;
+	garden.grid = blankGarden.grid;
+	garden.useGenes(individual);
+
+
+	unsigned short fitnes = 0;
+
+	for (size_t y = 0; y < Y; y++)
+	{
+		for (size_t x = 0; x < X; x++)
+		{
+			if (garden.grid[x][y] != 0 && garden.grid[x][y] != -1)
+			{
+				fitnes += 1;
+			}
+		}
+	}
+
+	return fitnes;
+}
+
+
+
+
+
+//nahodne vyberie 2 jedincov a toho, ktory ma vacs fitnes vrati
+Individual tournamentSelection(std::vector<Individual> population)
+{
+	
+	Individual individual1 = population[randIndex(1, populationSize)];
+
+	Individual individual2 = population[randIndex(1, populationSize)];
+
+	//kontrola, aby sa 2x nevybral ten isty jedinec
+	while (individual1 != individual2)
+	{
+		individual2 = population[randIndex(1, populationSize)];
+	}
+	
+
+	return (individual1.fitnesValue >= individual2.fitnesValue) ? individual1 : individual2;
+}
+
+
+/*Individual& rouletteSeleciton(std::vector<Individual> population)
+{
+	//population.erase(std::find(population.begin(), population.end()));
+}*/
+
+
+//funkcia zabezpecujuca krizenie, vrati 2 potomkov
+std::pair<Individual, Individual> crossover(Individual parent1, Individual parent2)
+{
+	//tu sa bude robit krizenie
+	unsigned short splitIndex = randIndex(1, individualSize - 1);
+
+	
+	std::vector<unsigned short> child1Genes(parent1.genes.begin(), parent1.genes.begin() + splitIndex);
+	std::vector<unsigned short> child2Genes(parent2.genes.begin(), parent2.genes.begin() + splitIndex);
+
+
+	for (auto& gene : child1Genes)
+	{
+		auto it = std::find(parent2.genes.begin(), parent2.genes.end(), gene);
+		if (it != parent2.genes.end())
+			parent2.genes.erase(it);
+	}
+
+	
+	child1Genes.insert(child1Genes.end(), parent2.genes.end() - (individualSize - child1Genes.size()), parent2.genes.end());
+
+
+	for (auto& gene : child2Genes)
+	{
+		auto it = std::find(parent1.genes.begin(), parent1.genes.end(), gene);
+		if (it != parent1.genes.end())
+			parent1.genes.erase(it);
+	}
+
+
+	child2Genes.insert(child2Genes.end(), parent1.genes.end() - (individualSize - child1Genes.size()), parent1.genes.end());
+
+
+	for (size_t i = 0; i < individualSize; i++)
+	{
+		if (std::find(child1Genes.begin(), child1Genes.end(), parent2.genes[i]) == child1Genes.end())
+			child1Genes.push_back(parent2.genes[i]);
+	}
+
+	for (size_t i = 0; i < individualSize; i++)
+	{
+		if (std::find(child2Genes.begin(), child2Genes.end(), parent1.genes[i]) == child2Genes.end())
+			child2Genes.push_back(parent1.genes[i]);
+	}
+
+
+	Individual child1;
+	Individual child2;
+
+	child1.genes = child1Genes;
+	child2.genes = child2Genes;
+
+
+	std::pair<Individual, Individual> children;
+	children = std::make_pair(child1, child2);
+
+	return children;
+}
+
+
+void mutate(const Individual& individual){}
+
+
+
+void loadBlankGenes()
+{
+	blankGenes = std::vector<unsigned short>((X + Y) * 2);
+	for (size_t i = 0; i < (X + Y) * 2; i++)
+	{
+		blankGenes[i] = i + 1;
+	}
+}
+
+
+//vytvorenie nahdonych genov pre jedinca z prvej generacie
+//celkovy pocet genov je X + Y + pocet kamenov
+std::vector<unsigned short> generateRandomGenes()
+{
+	std::vector<unsigned short> genes = blankGenes;
+
+	std::shuffle(genes.begin(), genes.end(), gen);
+
+	genes.resize(X + Y);
+
+	for (size_t i = 0; i < stoneCount; i++)
+	{
+		//nahodne vygenerovane pohyby 0 - vpravo, 1 - vlavo, ked narazi na prekazku
+		genes.push_back(randIndex(0, 1));
+	}
+
+	return genes;
+}
+
+
+//nahodne vytvori prvu generaciu jedincov - asi to bude std::vector<Genes>
+std::vector<Individual> firstGeneration()
+{
+	std::vector<Individual> firstGeneration;
+
+	for (size_t i = 0; i < populationSize; i++)
+	{
+		Individual newIndividual = Individual();
+		newIndividual.genes = generateRandomGenes();
+
+		firstGeneration.push_back(newIndividual);
+	}
+
+
+	return firstGeneration;
+}
+
+
+/*
+* 1 generacia bude 50 jedincov, lebo sak preco nie
+* v prvej generacii sa nahodne vygeneruju tieto jedince
+* najlepsich 10 sa vzdy dostane do dalsej generacie
+* 
+* krizenie - nahodne(podla ohodnotenia je vacsia alebo mensia sanca) sa nejak vyberie ze ktore sa s ktorymi krizia
+* 
+* mutacia - nahodne sa nejak skusi mutovat, to este neviem jak funguje :(
+*/
 
 /*Ak však príde k prekážke – kameòu alebo už pohrabanému piesku – musí sa otoèi, ak má kam.
 Ak má vo¾né smery v¾avo aj vpravo, je jeho vec, kam sa otoèí. Ak má vo¾ný len jeden smer, otoèí sa tam.
 Ak sa nemá kam otoèi, je koniec hry.*/
 int main()
 {
+	
 
-
-	//tak ako v zadani
+	//inicializacia
 	X = 12;
 	Y = 10;
 	
-	Garden garden = Garden(X, Y);
+	blankGarden = Garden(X, Y);
 
-	garden.placeStone(1, 2);
-	garden.placeStone(2, 4);
-	garden.placeStone(4, 3);
-	garden.placeStone(5, 1);
-	garden.placeStone(8, 6);
-	garden.placeStone(9, 6);
+	blankGarden.placeStone(1, 2);
+	blankGarden.placeStone(2, 4);
+	blankGarden.placeStone(4, 3);
+	blankGarden.placeStone(5, 1);
+	blankGarden.placeStone(8, 6);
+	blankGarden.placeStone(9, 6);
+
+	individualSize = X + Y + stoneCount;
+	desiredFitnesValue = X * Y - stoneCount;
+
+	loadBlankGenes();
+
+	//koniec inicializacie
+
+	firstGeneration();
+
+	Individual individual = Individual();
+
+	individual.genes = generateRandomGenes();
+	
+	//Individual genes = geneticAlgorithm();
+
+	blankGarden.useGenes(individual);
+
+	/*blankGarden.moveLeft(X - 1, 5, 1, individual);
+	blankGarden.moveLeft(X - 1, 8, 2, individual);*/
+
+	std::cout << blankGarden << std::endl;
 
 	
 	
-	
-	
-	std::cout << garden.grid << std::endl;
-
 	return 0;
 }
